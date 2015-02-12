8bd64ad63e0a4962794ea73f48e38bd154a9efef
alevellop <alevellop@gmail.com>
2015-02-12 23:02:08 +0100

    Refactor tests and validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -34,7 +34,7 @@ class ProductTest < ActiveSupport::TestCase
 								image_url: image_url)
 	end
 
-  test "iamge url" do
+  test "image url" do
   	ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
   	bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
