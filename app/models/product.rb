3b703b50db5120b375bcf6bb2391061b5c3d0d50
alevellop <alevellop@gmail.com>
2015-02-16 14:15:34 +0100

    Cart Creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
 
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	validates :title, length: {minimum: 10}
@@ -12,4 +16,17 @@ class Product < ActiveRecord::Base
 	def self.latest
 		Product.order(:updated_at).last
 	end
+
+	private
+
+		# ensure that there are no line items referencing this products
+		def ensure_not_referenced_by_any_line_item
+			
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
 end
